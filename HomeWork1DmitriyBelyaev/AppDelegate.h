48eee0d34e57830e3f98014b875983c478835a3b
Dmitriy Belyaev <avalonkl1@mail.com>
2016-04-26 16:40:01 -0700

    Initial Commit

---
+++ HomeWork1DmitriyBelyaev/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeWork1DmitriyBelyaev
+//
+//  Created by Dmitriy Belyaev on 26.04.16.
+//  Copyright © 2016 Dmitriy Belyaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeWork1DmitriyBelyaev/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HomeWork1DmitriyBelyaev
+//
+//  Created by Dmitriy Belyaev on 26.04.16.
+//  Copyright © 2016 Dmitriy Belyaev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
